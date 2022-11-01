2ff7dd5d83e9f479f439133188b6c333ca0a66d0
iosuAbal <jabal002@ikasle.ehu.eus>
2022-11-01 20:54:30 +0100

    amaituta

--- solid/src/lsp/ProjectFile.java
+++ solid/src/lsp/ProjectFile.java
@@ -1,6 +1,6 @@
 package lsp;
 
-public class ProjectFile implements iLoad{
+public abstract class ProjectFile implements iLoad{
 	protected String filepath;
 	public ProjectFile(String filePath) {
 		this.filepath=filePath;
